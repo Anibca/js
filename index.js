7dec02bb43a4f51e82ca70520e6c573a9f00e919
Anibca <38027956+Anibca@users.noreply.github.com>
2022-02-14 14:40:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-let count = 0
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+// document.getElementById("count").innerText = 5
 
-function increment() {
-    count += 1
-    countEl.textContent = count
-}
+// let count = 5
+// count = count + 1
+// console.log(count)
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContenttStr
-    countEl.textContent = 0
-    count = 0
-}
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-console.log("Let's count people on the subway!")
+
+
+
